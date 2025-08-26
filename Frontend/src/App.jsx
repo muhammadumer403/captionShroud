ac4c789c0289e2d41aa40d8a2fa3527fae5a95a9
Beast-52 <ara62580@gmail.com>
2025-08-26 21:18:10 +0500

    fet : routing & redux setup ✅

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,62 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Body from "./componets/Body";
-import Home from "./pages/Home";
-import About from "./pages/About";
+import PrivateRoute from "./componets/auth/PrivateRoute";
+import Upload from "./Pages/Upload";
+import Preview from "./Pages/Preview";
+import Editor from "./Pages/Editor";
+import Export from "./Pages/Export";
+import History from "./Pages/History";
+import SignUp from "./Pages/SignUp";
+import Login from "./Pages/Login";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Body />}>
-          <Route path="home" element={<Home />} />
-          <Route path="about" element={<About />} />
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<SignUp />} />
+          <Route
+            path="upload"
+            element={
+              <PrivateRoute>
+                <Upload />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="preview/:id"
+            element={
+              <PrivateRoute>
+                <Preview />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="editor/:id"
+            element={
+              <PrivateRoute>
+                <Editor />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="export/:id"
+            element={
+              <PrivateRoute>
+                <Export />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="history"
+            element={
+              <PrivateRoute>
+                <History />
+              </PrivateRoute>
+            }
+          />
         </Route>
       </Routes>
     </Router>
